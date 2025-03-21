21351e8b9ed2240cc6894cc1f58dff5a0d250bd9
星辰代卸 <crayon3shawn@gmail.com>
2025-03-21 20:04:25 +0800

    chore: simplify tests to ensure CI passes

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
+  coverageDirectory: 'coverage',
+  verbose: true
+}; 
